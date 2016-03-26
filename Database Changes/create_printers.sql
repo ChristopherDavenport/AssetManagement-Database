2343321604be075a4549c0ec52a9e3b78ac072f9
Chris Davenport <ChristopherDavenport@outlook.com>
2016-03-26 13:05:20 -0400

    Updated Printers for Standard Table Format

--- Database Changes/create_printers.sql
+++ Database Changes/create_printers.sql
@@ -1,23 +1,29 @@
 CREATE TABLE printers
-(printer_pk       varchar(20)   not null,
- printer_desc     varchar(120)  not null,
- status_ck        varchar(1)    not null,
- status_date      date           not null,
- pages_per_min    SMALLINT,
- charge_back      DECIMAL(7,2)    not null,
- activity_date    date           not null,
- user_id          varchar(30)   not null
+( id               BIGINT                       not null,
+  printer_pk       varchar(20)                  not null,
+  printer_desc     varchar(120)                 not null,
+  pages_per_min    SMALLINT,
+  charge_back      DECIMAL(7,2)                 not null,
+
+  status_ck        varchar(1)                   not null,
+  status_date      TIMESTAMP with time zone     not null,
+  activity_user   varchar(30)                   not null,
+  activity_date    TIMESTAMP with time zone     not null,
+  created_user    varchar(30)                   not null,
+  created_date    varchar(30)                   not null
 );
 
-ALTER TABLE printers ADD CONSTRAINT pk_printers PRIMARY KEY (printer_pk);
+ALTER TABLE printers ADD CONSTRAINT id_printers PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX pk_printers ON printers (printer_pk);
 
 ALTER TABLE printers ADD CONSTRAINT ck_printers_status CHECK (status_ck in ('A', 'I'));
 
 CREATE FUNCTION tiu_printers() RETURNS TRIGGER AS $$
  BEGIN
-  new.activity_date := current_date;
-  new.user_id       := user;
-  new.printer_pk     := upper(new.printer_pk);
+  new.activity_date := current_timestamp;
+  new.activity_user := user;
+  new.printer_pk    := upper(new.printer_pk);
   new.status_ck     := upper(coalesce(new.status_ck, 'A'));
   IF (old.status_ck <> new.status_ck) || old.status_ck IS NULL THEN
    new.status_date  := current_date;
@@ -25,5 +31,15 @@ CREATE FUNCTION tiu_printers() RETURNS TRIGGER AS $$
  END;
  $$ LANGUAGE plpgsql;
 
+CREATE FUNCTION ti_printers() RETURNS TRIGGER AS $$
+  BEGIN
+    new.created_date := current_timestamp;
+    new.created_user := user;
+  END;
+  $$ LANGUAGE plpgsql;
+
 CREATE TRIGGER tiu_printers BEFORE INSERT OR UPDATE ON printers
  FOR EACH ROW EXECUTE PROCEDURE tiu_printers();
+
+CREATE TRIGGER ti_printers BEFORE INSERT ON printers
+  FOR EACH ROW EXECUTE PROCEDURE ti_printers();
